a28c059f9009936134426ef4b753d0467237b17b
Fourth <sirawit@dek-d.com>
2021-08-06 12:55:02 +0700

    Test Run App E2E

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,9 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/posts/new"); // 1.
-    cy.get("input.post-title") // 2.
-      .type("My First Post"); // 3.
+describe("Test Run App", () => {
+  it("Visits the app root url('/')", () => {
+    cy.visit("/");
   });
 });
